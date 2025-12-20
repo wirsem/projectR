b2454e0488e90950b9db86c61e3b57886427ac19
Wira Sembiring <wira.timothy47@gmail.com>
2025-12-20 12:07:43 +0700

    tes

---
+++ visualisasi rules dengan graph.R
@@ -0,0 +1,5 @@
+library(arules)
+library(arulesViz)
+transaksi <- read.transactions(file="https://storage.googleapis.com/dqlab-dataset/data_transaksi.txt", format="single", sep="\t", cols=c(1,2), skip=1)
+mba <- apriori(transaksi,parameter = list(supp = 0.1, confidence = 0.5))
+plot(subset(mba, lift>1.1), method="graph")
